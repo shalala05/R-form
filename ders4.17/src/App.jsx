0c575b677c1745959aa86318348533a9cefe2452
shalala05 <allahverdiyevashelale05@gmail.com>
2024-12-19 17:13:11 +0400

    4.17(first)

---
+++ ders4.17/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import EmployeesList from './EmployeesList.jsx';
+
+const App = () => {
+  return (
+    <div>
+      <EmployeesList/>
+    </div>
+  );
+};
+
+export default App;
