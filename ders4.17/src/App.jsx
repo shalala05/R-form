ba699571593458e9d7926978665b752272bb71a8
shalala05 <allahverdiyevashelale05@gmail.com>
2024-12-21 02:04:49 +0400

    ders4.17(second)

--- ders4.17/src/App.jsx
+++ ders4.17/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './Navbar';
-import Clock from './Clock';
-import Stopwatch from './Stopwatch';
-import Timer from './Timer';
+import Navbar from "./components/Navbar";
+import Clock from './components/Clock';
+import Stopwatch from './components/Stopwatch';
+import Timer from './components/Timer';
 
 const App = () => {
   return (
