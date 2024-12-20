de162f99c711477dee622bcf1b42140ce80ad6f2
shalala05 <allahverdiyevashelale05@gmail.com>
2024-12-20 15:11:14 +0400

    ders4.9

--- ders4.9/src/main.jsx
+++ ders4.9/src/main.jsx
@@ -1,8 +1,10 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-root.render(<App />);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
